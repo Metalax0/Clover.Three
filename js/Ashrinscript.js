a909763ae827b5e42ee041fd5b9a0812b0ab551e
Ashrin K.C <ashrinkc3@gmail.com>
2021-11-18 18:50:44 +0545

    added ashrinscript and made few changes in ashrin.html and ashrincss

---
+++ js/Ashrinscript.js
@@ -0,0 +1,20 @@
+
+    var p1 = document.querySelector("#p1");
+    var p2 = document.querySelector("#p2");
+    var p3 = document.querySelector("#p3");
+    var p4 = document.querySelector("#p4");
+    var p5 = document.querySelector("#p5");
+    var p6 = document.querySelector("#p6");
+    var p7 = document.querySelector("#p7");
+    var p8 = document.querySelector("#p8");
+    var p9 = document.querySelector("#p9");
+    const gg = ( [p1,p2,p3],[p1,p4,p7],[p1,p5,p9]);
+    p1.addEventListener('click',(e)=>{
+        document.getElementById("p1").innerHTML = "X";  
+    })
+    p2.addEventListener('click',(e)=>{
+    document.getElementById("p2").innerHTML = "X";
+    })
+    for(let i=0;i<=7;i++){
+
+    }
